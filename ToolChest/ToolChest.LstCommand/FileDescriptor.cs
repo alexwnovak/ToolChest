e7fb9a83347a91ea2fb49422f2f39b91d2522ca7
Alex Novak <alexwnovak@gmail.com>
2017-02-10 12:44:56 -0500

    +FileDescriptor struct
    This describes a file entry, but isn't a FileInfo[]. It might make sense to use that in the future, but for now it doesn't.

---
+++ ToolChest/ToolChest.LstCommand/FileDescriptor.cs
@@ -0,0 +1,21 @@
+﻿namespace ToolChest.LstCommand
+{
+   public struct FileDescriptor
+   {
+      public string FullPath
+      {
+         get;
+      }
+
+      public long Size
+      {
+         get;
+      }
+
+      public FileDescriptor( string fullPath, long size )
+      {
+         FullPath = fullPath;
+         Size = size;
+      }
+   }
+}
