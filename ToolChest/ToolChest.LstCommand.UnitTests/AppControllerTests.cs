a714eaa4fb9c35efa096f8f1d313292b51e93eb5
Alex Novak <alexwnovak@gmail.com>
2017-02-09 22:57:02 -0500

    AppController now enumerates the files in the current directory

--- ToolChest/ToolChest.LstCommand.UnitTests/AppControllerTests.cs
+++ ToolChest/ToolChest.LstCommand.UnitTests/AppControllerTests.cs
@@ -1,6 +1,33 @@
-﻿namespace ToolChest.LstCommand.UnitTests
+﻿using Xunit;
+using Moq;
+using SystemWrapper;
+using SystemWrapper.IO;
+using ToolChest.LstCommand.UnitTests.Helpers;
+
+namespace ToolChest.LstCommand.UnitTests
 {
    public class AppControllerTests
    {
+      [Fact]
+      public void Start_CurrentDirectoryHasOneFile_PrintsOneFile()
+      {
+         var paths = ArrayHelper.Create( "File.txt" );
+
+         // Arrange
+         
+         var directoryMock = new Mock<IDirectoryWrap>();
+         directoryMock.Setup( d => d.GetFiles( "." ) ).Returns( paths );
+         var consoleMock = new Mock<IConsoleWrap>();
+
+         // Act
+
+         var appController = new AppController( directoryMock.Object, consoleMock.Object );
+
+         appController.Start();
+
+         // Assert
+
+         consoleMock.Verify( c => c.WriteLine( paths[0] ), Times.Once );
+      }
    }
 }
--- ToolChest/ToolChest.LstCommand/AppController.cs
+++ ToolChest/ToolChest.LstCommand/AppController.cs
@@ -1,6 +1,27 @@
-﻿namespace ToolChest.LstCommand
+﻿using SystemWrapper;
+using SystemWrapper.IO;
+
+namespace ToolChest.LstCommand
 {
    public class AppController
    {
+      private readonly IDirectoryWrap _directory;
+      private readonly IConsoleWrap _console;
+
+      public AppController( IDirectoryWrap directory, IConsoleWrap console )
+      {
+         _directory = directory;
+         _console = console;
+      }
+
+      public void Start()
+      {
+         var files = _directory.GetFiles( "." );
+
+         foreach ( string file in files )
+         {
+            _console.WriteLine( file );
+         }
+      }
    }
 }
