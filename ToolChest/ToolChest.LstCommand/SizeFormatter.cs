85b5433e10102e92df948381d42576fe8c6dec69
Alex Novak <alexwnovak@gmail.com>
2017-02-10 22:39:17 -0500

    SizeFormatter now returns a 1-to-3 digit size post-fixed with its units

--- ToolChest/ToolChest.LstCommand/SizeFormatter.cs
+++ ToolChest/ToolChest.LstCommand/SizeFormatter.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace ToolChest.LstCommand
+﻿namespace ToolChest.LstCommand
 {
    public static class SizeFormatter
    {
       public static string Format( long size )
       {
-         throw new NotImplementedException();
+         if ( size < 1000 )
+         {
+            return $"{size,3} B ";
+         }
+         if ( size < 1000000 )
+         {
+            return $"{size / 1000,3} KB";
+         }
+         if ( size < 1000000000 )
+         {
+            return $"{size / 1000000,3} MB";
+         }
+         if ( size < 1000000000000 )
+         {
+            return $"{size / 1000000000,3} GB";
+         }
+
+         return $"{size / 1000000000000,3} TB";
       }
    }
 }
