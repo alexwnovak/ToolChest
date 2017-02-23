91335a3222bf896841c5ea084e247bc2db9de135
Alex Novak <alexwnovak@gmail.com>
2017-02-22 20:37:18 -0500

    +ColorProvider lookup class for relating extensions to colors
    In the future this should have the idea of groups, probably, since similar things should be colored the same (docs, media files, executables, whatever).

---
+++ ToolChest/ToolChest.LstCommand/ColorProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToolChest.LstCommand
+{
+   public static class ColorProvider
+   {
+      private static readonly Dictionary<string, ConsoleColor> _colorTable = CreateColorTable();
+
+      private static Dictionary<string, ConsoleColor> CreateColorTable()
+      {
+         var colorTable = new Dictionary<string, ConsoleColor>();
+
+         colorTable[".doc"] = ConsoleColor.Blue;
+         colorTable[".docx"] = ConsoleColor.Blue;
+         colorTable[".txt"] = ConsoleColor.Blue;
+         colorTable[".md"] = ConsoleColor.Blue;
+
+         colorTable[".bat"] = ConsoleColor.Green;
+         colorTable[".com"] = ConsoleColor.Green;
+         colorTable[".exe"] = ConsoleColor.Green;
+         colorTable[".ps1"] = ConsoleColor.Green;
+
+         return colorTable;
+      }
+
+      public static ConsoleColor GetColor( string extension )
+      {
+         extension = extension.ToLower();
+         ConsoleColor color;
+
+         if ( !_colorTable.TryGetValue( extension, out color ) )
+         {
+            return ConsoleColor.Gray;
+         }
+
+         return color;
+      }
+   }
+}
