957fbdaeb84160d2f70c85c2d1cbc4d37b58dd29
Alex Novak <alexwnovak@gmail.com>
2017-03-16 21:41:39 -0400

    +Internal Win32 static class for pinvoke

---
+++ ToolChest/ToolChest.VuCommand/Internal/Win32.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ToolChest.VuCommand.Internal
+{
+   internal static class Win32
+   {
+      [DllImport( "kernel32.dll" )]
+      internal static extern IntPtr GetStdHandle( uint nStdHandle );
+
+      internal const uint StdOutputHandle = 0xfffffff5;
+   }
+}
