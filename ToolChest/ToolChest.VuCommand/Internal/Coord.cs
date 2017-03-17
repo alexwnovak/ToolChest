571a108e678fa5c927e279b7f1011d5cfd6cf85f
Alex Novak <alexwnovak@gmail.com>
2017-03-16 22:08:53 -0400

    +Coord struct

---
+++ ToolChest/ToolChest.VuCommand/Internal/Coord.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ToolChest.VuCommand.Internal
+{
+   [StructLayout( LayoutKind.Sequential )]
+   internal struct Coord
+   {
+      public short X;
+      public short Y;
+   }
+}
