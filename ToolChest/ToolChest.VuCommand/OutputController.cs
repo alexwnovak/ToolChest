85ae6eee446762f6f5b743ecc06e27ee1b027c04
Alex Novak <alexwnovak@gmail.com>
2017-03-16 20:30:52 -0400

    DisplaySyntax now displays syntax

--- ToolChest/ToolChest.VuCommand/OutputController.cs
+++ ToolChest/ToolChest.VuCommand/OutputController.cs
@@ -1,9 +1,18 @@
-﻿namespace ToolChest.VuCommand
+﻿using System;
+
+namespace ToolChest.VuCommand
 {
    public class OutputController : IOutputController
    {
       public void DisplaySyntax()
       {
+         Console.ForegroundColor = ConsoleColor.Green;
+         Console.Write( "Syntax: " );
+
+         Console.ForegroundColor = ConsoleColor.Gray;
+         Console.WriteLine( "vu <file>" );
+
+         Console.WriteLine();
       }
    }
 }
