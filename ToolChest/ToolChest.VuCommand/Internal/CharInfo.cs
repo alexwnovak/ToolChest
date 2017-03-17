8a0987157e7c8ab0e781f3ce989885166f310b50
Alex Novak <alexwnovak@gmail.com>
2017-03-16 22:11:04 -0400

    +CharInfo struct

---
+++ ToolChest/ToolChest.VuCommand/Internal/CharInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ToolChest.VuCommand.Internal
+{
+   [StructLayout( LayoutKind.Explicit )]
+   internal struct CharInfo
+   {
+      [FieldOffset( 0 )]
+      public char UnicodeChar;
+      [FieldOffset( 0 )]
+      public char AsciiChar;
+      [FieldOffset( 2 )]
+      public ushort Attributes;
+   }
+}
