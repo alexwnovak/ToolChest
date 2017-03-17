38791e1fd24436bdeacbca3b19040829265297da
Alex Novak <alexwnovak@gmail.com>
2017-03-16 22:11:44 -0400

    +Import for WriteConsoleOutput

--- ToolChest/ToolChest.VuCommand/Internal/Win32.cs
+++ ToolChest/ToolChest.VuCommand/Internal/Win32.cs
@@ -5,6 +5,13 @@ namespace ToolChest.VuCommand.Internal
 {
    internal static class Win32
    {
+      [DllImport( "kernel32.dll", EntryPoint = "WriteConsoleOutputW", CharSet = CharSet.Unicode, SetLastError = true )]
+      internal static extern bool WriteConsoleOutput( IntPtr hConsoleOutput,
+      [MarshalAs( UnmanagedType.LPArray ), In] CharInfo[] lpBuffer,
+         Coord dwBufferSize,
+         Coord dwBufferCoord,
+         ref Rect lpWriteRegion );
+
       [DllImport( "kernel32.dll" )]
       internal static extern IntPtr GetStdHandle( uint nStdHandle );
 
