d0535a6c64f5fa0f8dc7424c2ecb8d46bd98d33b
Alex Novak <alexwnovak@gmail.com>
2017-03-19 19:48:48 -0400

    Can now read the current drive's free space

--- ToolChest/ToolChest.SpaceCommand/DiskSpaceReader.cs
+++ ToolChest/ToolChest.SpaceCommand/DiskSpaceReader.cs
@@ -1,10 +1,23 @@
-﻿namespace ToolChest.SpaceCommand
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ToolChest.SpaceCommand
 {
    public class DiskSpaceReader : IDiskSpaceReader
    {
+      [DllImport( "kernel32.dll", SetLastError = true, CharSet = CharSet.Auto )]
+      private static extern bool GetDiskFreeSpaceEx( string lpDirectoryName,
+         out ulong lpFreeBytesAvailable,
+         out ulong lpTotalNumberOfBytes,
+         out ulong lpTotalNumberOfFreeBytes );
+
       public long GetFreeDiskSpace()
       {
-         throw new System.NotImplementedException();
+         ulong _, freeSpace;
+
+         GetDiskFreeSpaceEx( Environment.CurrentDirectory, out _, out _, out freeSpace );
+
+         return (long) freeSpace;
       }
    }
 }
